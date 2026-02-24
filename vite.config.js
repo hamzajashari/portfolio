8a53227946aa4e387161e371d582e8c453355784
Hamza Jashari <hamza.jashari@evn.mk>
2026-02-24 13:02:17 +0100

    fix base route

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/portfolio/",
+  base: process.env.VITE_BASE || "/",
   plugins: [react()],
   resolve: {
     alias: {
