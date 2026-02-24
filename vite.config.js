956ba899ccdefb816c4bf67fe383653864818ac4
Hamza Jashari <hamza.jashari@evn.mk>
2026-02-24 18:21:05 +0100

    gh-paghes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: process.env.VITE_BASE || "/",
+  base: process.env.BASE || "/",
   plugins: [react()],
   resolve: {
     alias: {
