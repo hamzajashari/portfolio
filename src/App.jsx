5f7ca05fcec04aee5c1fe98c8fec4b294d86620d
Hamza Jashari <hamza.jashari@evn.mk>
2026-03-02 07:52:41 +0100

    remove cursor

--- src/App.jsx
+++ src/App.jsx
@@ -11,23 +11,11 @@ import {
 } from "./components";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import AnimatedCursor from "react-animated-cursor";
 import { Meteors } from "./components/ui/meteors";
 
 function App() {
   return (
     <>
-      <AnimatedCursor
-        color="255,255,255"
-        innerSize={8}
-        outerSize={35}
-        innerScale={1}
-        outerScale={1.7}
-        outerAlpha={0}
-        outerStyle={{
-          border: "3px solid #fff",
-        }}
-      />
       <BrowserRouter>
         <ToastContainer />
         <div className="relative z-10 bg-primary">
